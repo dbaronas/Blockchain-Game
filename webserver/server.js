ff6e530ad6618d50440c85ca57c2d99852066889
Martynas M <muizysmartynas@gmail.com>
2023-02-11 13:20:19 +0200

    fixing issues

--- webserver/server.js
+++ webserver/server.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const dotenv = require('dotenv')
 const app = express()
-dotenv.config()
+const PORT = 3000
 
 app.get('/start', (req, res) => {
     res.send('TEST')
 })
 
-app.listen(process.env.PORT)
+app.listen(PORT, () => {
+    console.log('Server runing on port 3000...')
+})
