318c18ba8b23547cfcefbaeaa2b65001d69ca391
Martynas M <muizysmartynas@gmail.com>
2023-03-06 18:25:23 +0200

    connected backend with blockchain

--- backend-webserver/app/ERC1155.js
+++ backend-webserver/app/ERC1155.js
@@ -1,3 +1,12 @@
 require('dotenv').config()
 const Web3 = require('web3')
-const web3 = new Web3(new Web3.providers.HttpProvider(process.env.BLOCKCHAIN_RPC))
+const Contract = require('web3-eth-contract')
+Contract.setProvider(new Web3.providers.HttpProvider(process.env.BLOCKCHAIN_RPC))
+const abi = require('../ABI/GameItem.json').abi
+const contract = new Contract(abi, process.env.ERC1155)
+
+console.log(contract)
+
+module.exports = {
+
+}
--- backend-webserver/app/ERC20.js
+++ backend-webserver/app/ERC20.js
@@ -1,4 +1,17 @@
 require('dotenv').config()
 const Web3 = require('web3')
-const web3 = new Web3(new Web3.providers.HttpProvider(process.env.BLOCKCHAIN_RPC))
+const Contract = require('web3-eth-contract')
+Contract.setProvider(new Web3.providers.HttpProvider(process.env.BLOCKCHAIN_RPC))
+const abi = require('../ABI/PoseidonToken.json').abi
+const contract = new Contract(abi, process.env.ERC20)
+
+async function main() {
+    console.log(await contract.methods.name().call())
+}
+
+main()
+
+module.exports = {
+    
+}
 
