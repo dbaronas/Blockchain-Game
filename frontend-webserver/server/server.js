2dc0e11c24ab8a6a3699f1a766e66ef957b89fb8
Martynas M <muizysmartynas@gmail.com>
2023-03-12 16:33:55 +0200

    website connected to the game

---
+++ frontend-webserver/server/server.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const PORT = 3000
+var players = {}
+
+
+//test
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('../public'))
+app.use(express.static('../dist'))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '../dist/index.html')
+})
+app.get('/game', (req, res) => {
+    res.sendFile(__dirname + '../public/game/index.html')
+})
+
+
+server.listen(PORT, () => {
+    console.log('3000')
+})
+
+var players = {}
+
+io.on('connection', function (socket) {
+    console.log('a user connected: ', socket.id)
+    players[socket.id] = {
+      x: 500,
+      y: 500,
+      playerId: socket.id,
+      animation: 'idle'
+    }
+    socket.emit('currentPlayers', players)
+    socket.broadcast.emit('newPlayer', players[socket.id])
+  
+    socket.on('disconnect', function () {
+      console.log('user disconnected: ', socket.id)
+      delete players[socket.id]
+      io.emit('disc', socket.id)
+    })
+  
+    socket.on('playerMovement', function (movementData) {
+      players[socket.id].x = movementData.x
+      players[socket.id].y = movementData.y
+      players[socket.id].animation = movementData.animation
+      socket.broadcast.emit('playerMoved', players[socket.id])
+      socket.broadcast.emit('playerAnimation', players[socket.id])
+    })
+})
