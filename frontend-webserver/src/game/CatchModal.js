efe95c35ecd3fd4b160abca2c372b0032e2704c6
Martynas M <muizysmartynas@gmail.com>
2023-03-20 18:14:24 +0200

    modal pop up animation

--- frontend-webserver/src/game/CatchModal.js
+++ frontend-webserver/src/game/CatchModal.js
@@ -1,11 +1,10 @@
-import Phaser from 'phaser';
 export default class CatchModal extends Phaser.Scene {
     constructor() {
         super('modal')
         this.x = 0
         this.y = 0
-        this.width = 200
-        this.heigth = 200
+        this.width = 0
+        this.heigth = 0
     }
 
     static preload(scene) {
@@ -17,13 +16,19 @@ export default class CatchModal extends Phaser.Scene {
     create() {
         var modal = this.add.image(0, 0, 'modal')
         var button = this.add.image(0, 0, 'button').setInteractive().setScale(0.5).setOrigin(0.5, 0)
-        var buttonText = this.add.text(0, 0, 'MINT').setOrigin(0.5, -1).setFontSize(40)
+        var buttonText = this.add.text(0, 0, 'MINT').setOrigin(0.5, -0.8).setFontSize(40)
         var text = this.add.text(0, 0, 'YOU GOT A FISH GG!').setOrigin(0.5, 2).setFontSize(20)
 
         this.container = this.add.container(650, 200, [modal, button, buttonText, text])
-
+        this.container.setScale(0)
+        this.tween = this.tweens.add({
+            targets: this.container,
+            scale: 1,
+            ease: 'Back',
+            duration: 200
+        })
+        
         button.on('pointerdown', () => {
-            console.log('nft minted ggwp soldier!')
             this.scene.stop()
         })
         
