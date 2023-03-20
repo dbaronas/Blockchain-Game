ea52978752148edbff233418beb14c5630e101f3
Martynas M <muizysmartynas@gmail.com>
2023-03-20 21:46:12 +0200

    testing post request from game + nfts

--- backend-webserver/app/ERC721.js
+++ backend-webserver/app/ERC721.js
@@ -1,22 +1,25 @@
 require('dotenv').config()
 const Web3 = require('web3')
 const Contract = require('web3-eth-contract')
-Contract.setProvider(new Web3.providers.HttpProvider(process.env.BLOCKCHAIN_RPC), (error) => {
-    if(error) {
-        console.log(`Couldn't connect to `)
-    }
-})
+Contract.setProvider(new Web3.providers.HttpProvider(process.env.BLOCKCHAIN_RPC))
 const abi = require('../ABI/PoseidonNFT.json').abi
 const contract = new Contract(abi, process.env.ERC721)
 
 const mint = async(req, res) => {
-    const uri = `${process.env.IP}/gameitems/SFTs/metadata/fr_1.json`
-    const address = '0x0746A08dB469275d7db9EAFDF6Bca5BA78403b4b'
-    await contract.methods.mint(address, uri).send({from: process.env.OWNER, gas: 216000}).then((result) => {
+    const address = req.body.address
+    const item_id = req.body.id
+    const uri = `${process.env.IP}/gameitems/NFTs/metadata/${id}.json`
+    const data = {
+        address: address,
+        id: item_id
+    }
+    //const address = '0x0746A08dB469275d7db9EAFDF6Bca5BA78403b4b'
+    res.json({data})
+    /*await contract.methods.mint(address, uri).send({from: process.env.OWNER, gas: 216000}).then((result) => {
         res.json(result)
     }).catch((error) => {
         res.json({error: '' + error})
-    })
+    })*/
 }
 
 const ownerOf = async(req, res) => {
