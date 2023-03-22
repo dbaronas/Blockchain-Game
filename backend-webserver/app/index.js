0fecd26055e49af3fe5492e7bf22367d32fe6c37
Baronas Dominykas <dominykas.baronas@mif.stud.vu.lt>
2023-03-23 01:41:50 +0200

    Added a function to retrieve data from database
    Added a function to retrieve data from database table News.

--- backend-webserver/app/index.js
+++ backend-webserver/app/index.js
@@ -1,9 +1,11 @@
 //const erc20 = require('./ERC20')
 const erc721 = require('./ERC721')
+const db = require('./databaseQueries')
 //const erc1155 = require('./ERC1155')
 
 module.exports = {
     //erc20,
     erc721,
+    db
     //erc1155
 }
