1517c905207e1ad0c34ab4f885b364e43889c9a4
Martynas M <muizysmartynas@gmail.com>
2023-04-10 02:51:40 +0300

    fixing t letter not working

--- frontend-webserver/src/game/ChatScene.js
+++ frontend-webserver/src/game/ChatScene.js
@@ -5,11 +5,11 @@ export default class ChatScene extends Phaser.Scene {
     }
 
     preload(scene) {
-        this.load.html('input','assets/chat_input.html')
+        this.load.html('input', 'assets/chat_input.html')
     }
 
     create(data) {
-        var { scene, io} = data
+        var { scene, io } = data
         this.isFocused = false
         this.chatBox = this.add.rectangle(0, 0, 370, 200, 0x964B00)
         this.chatBox.alpha = 0.2
@@ -21,7 +21,7 @@ export default class ChatScene extends Phaser.Scene {
             fontStyle: 'bold'
         })
         this.container = this.add.container(185, 580, [this.chatBox, this.chat])
-        let chat = this.chatInput.getChildByName('input') 
+        let chat = this.chatInput.getChildByName('input')
         let send = this.chatInput.getChildByName('send')
         this.chatMessages.push('Welcome to MetaOcean!')
         this.chat.setText(this.chatMessages)
@@ -31,17 +31,16 @@ export default class ChatScene extends Phaser.Scene {
         this.chatButton = this.input.keyboard.addKey('T')
 
         this.chatButton.on('down', () => {
-            if(this.isFocused === false) {
+            if (this.isFocused === false) {
                 chat.focus()
                 scene.input.keyboard.enabled = false
                 scene.input.keyboard.disableGlobalCapture()
-                this.isFocused = true
             }
         })
 
 
         this.input.keyboard.on('keydown-ESC', () => {
-            if(this.isFocused === true) {
+            if (this.isFocused === true) {
                 chat.blur()
                 scene.input.keyboard.enabled = true
                 scene.input.keyboard.enableGlobalCapture()
@@ -49,7 +48,7 @@ export default class ChatScene extends Phaser.Scene {
             }
         })
         this.input.keyboard.on('keydown-ENTER', () => {
-            if(chat.value !== '' && this.isFocused === true) {
+            if (chat.value !== '' && this.isFocused === true) {
                 send.click()
                 chat.blur()
                 scene.input.keyboard.enabled = true
@@ -59,22 +58,22 @@ export default class ChatScene extends Phaser.Scene {
         })
 
         this.chatInput.on('click', event => {
-            if(event.target.name === 'send') {
-                if(chat.value !== '') {
+            if (event.target.name === 'send') {
+                if (chat.value !== '') {
                     io.emit('message', chat.value)
                     chat.value = ''
                 }
                 scene.input.keyboard.enabled = true
                 scene.input.keyboard.enableGlobalCapture()
-            } else if(event.target.name === 'input') {
+            } else if (event.target.name === 'input') {
                 scene.input.keyboard.enabled = false
-                scene.input.keyboard.disableGlobalCapture() 
-                }
+                scene.input.keyboard.disableGlobalCapture()
+            }
         })
 
         io.on('messageResponse', (message) => {
             this.chatMessages.push(io.id + ': ' + message)
-            if(this.chatMessages.length > 9) {
+            if (this.chatMessages.length > 9) {
                 this.chatMessages.shift()
                 this.chat.setText(this.chatMessages)
             } else {
@@ -82,13 +81,16 @@ export default class ChatScene extends Phaser.Scene {
                 this.chat.setText(this.chatMessages)
             }
         })
-        
+
     }
 
     update() {
 
-        if(Phaser.Input.Keyboard.JustDown(this.chatButton)) {
-            this.chatInput.getChildByName('input').value = this.chatInput.getChildByName('input').value.substring(0, this.chatInput.getChildByName('input').value.length - 1)
+        if (this.isFocused === false) {
+            if (Phaser.Input.Keyboard.JustDown(this.chatButton)) {
+                this.chatInput.getChildByName('input').value = this.chatInput.getChildByName('input').value.substring(0, this.chatInput.getChildByName('input').value.length - 1)
+                this.isFocused = true
+            }
         }
     }
 }
