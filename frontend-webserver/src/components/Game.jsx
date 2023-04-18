634d0bfd942eac20a2edd7f9d830619d98ed3a18
Martynas M <muizysmartynas@gmail.com>
2023-04-18 19:58:02 +0300

    deleting not needed file

--- frontend-webserver/src/components/Game.jsx
+++
@@ -1,9 +0,0 @@
-const Game = () => (
-    <div>
-        <script src="//cdn.jsdelivr.net/npm/phaser@3.55.2/dist/phaser.min.js" defer></script>
-        <div id="fisherman"></div>
-        <script type="module" src="js/game.js" async></script>
-    </div>
-  )
-
-export default Game
