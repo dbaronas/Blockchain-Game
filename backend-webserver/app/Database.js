11f54d9f19b3146bd5262f856a40251fc97849bc
Martynas M <muizysmartynas@gmail.com>
2023-04-18 11:59:56 +0300

    optimizing checkUser function

--- backend-webserver/app/Database.js
+++ backend-webserver/app/Database.js
@@ -8,15 +8,8 @@ const getNews = async(req, res) => {
 const checkUser = async(req, res) => {
     const { type, data } = req.body
 
-    if(type == 'address') {
-        const user = await db.User.findOne({where: {wallet_address: data}})
-        if(user) {
-            res.json({exists: true})
-        } else {
-            res.json({exists: false})
-        }
-    } else if(type == 'username') {
-        const user = await db.User.findOne({where: {username: data}})
+    if(type == 'wallet_address' || type == 'username') {
+        const user = await db.User.findOne({where: {[type]: data}})
         if(user) {
             res.json({exists: true})
         } else {
