df684f38874a920a0614160aba1dfe3e2fcce635
Martynas M <muizysmartynas@gmail.com>
2023-04-20 13:44:53 +0300

    Update server.js

--- backend-webserver/server.js
+++ backend-webserver/server.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 
 //require('./middleware/permissions')
 var corsOptions = {
-    origin: 'http://193.219.91.103:4362',
+    origin: ['http://193.219.91.103:4362', '10.0.0.107'],
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type'],
     credentials: true
