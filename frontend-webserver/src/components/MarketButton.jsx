b875668a9a9cc9765e5efbd0809d519829508c7a
Martynas M <muizysmartynas@gmail.com>
2023-04-26 23:58:12 +0300

    buttons

--- frontend-webserver/src/components/MarketButton.jsx
+++ frontend-webserver/src/components/MarketButton.jsx
@@ -3,6 +3,7 @@ const MarketButton = ({ styles }) => {
     <button
       type="button"
       className={`py-4 px-6 bg-gold-gradient font-poppins font-medium text-[1.1rem] text-primary outline-none ${styles} rounded-[0.7rem]`}
+      href={'/marketplace'}
     >
       Shop Now!
     </button>
--- frontend-webserver/src/components/NewsButton.jsx
+++ frontend-webserver/src/components/NewsButton.jsx
@@ -3,6 +3,7 @@ const NewsButton = ({ styles }) => {
     <button
       type="button"
       className={`py-4 px-6 bg-gold-gradient font-poppins font-medium text-[1.1rem] text-primary outline-none ${styles} rounded-[0.7rem]`}
+      href={'/news'}
     >
       Patch Notes
     </button>
