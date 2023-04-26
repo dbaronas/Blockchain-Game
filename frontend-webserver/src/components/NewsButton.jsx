9a7a3c8cecb773f44d1d679dff0508a202cb2a6a
Martynas M <muizysmartynas@gmail.com>
2023-04-27 00:04:18 +0300

    fixing bug

--- frontend-webserver/src/components/NewsButton.jsx
+++ frontend-webserver/src/components/NewsButton.jsx
@@ -5,7 +5,9 @@ const NewsButton = ({ styles }) => {
       className={`py-4 px-6 bg-gold-gradient font-poppins font-medium text-[1.1rem] text-primary outline-none ${styles} rounded-[0.7rem]`}
       href={'/news'}
     >
-      Patch Notes
+      <a href="/news">
+        Patch Notes
+      </a>
     </button>
   );
 };
