dcccc1ace99e83eb0a1c73a2145fc6ae45ba3eb7
Arminas Attas <armisatas@gmail.com>
2023-05-01 23:46:13 +0300

    French play button size change

--- frontend-webserver/src/components/PlayNow.jsx
+++ frontend-webserver/src/components/PlayNow.jsx
@@ -3,13 +3,14 @@ import { useTranslation } from "react-i18next"
 
 const PlayNow = () => {
 
-  const { t } = useTranslation('Hero')
+  const { t, i18n } = useTranslation('Hero')
+  const fontSize = i18n.language === "French" ? "text-[26px] leading-[18px]" : "text-[40px] leading-[24px]"
 
   return(
     <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-gold-gradient p-[4px] cursor-pointer`}>
       <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
         <div className={`${styles.flexStart} flex-row`}>
-          <p className="font-vt323 font-medium text-[40px] leading-[24px] mb-1 text-center">
+          <p className={`font-vt323 font-medium ${fontSize} mb-1 text-center`}>
             <a href="/game">
               <span className="text-gold hover:text-secondary leading-8">
                 {t("Play")}<br/>{t("Now")}
