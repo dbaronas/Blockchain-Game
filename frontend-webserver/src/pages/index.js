2b39c47763bd95636316f71c019c848c39d171c4
Baronas Dominykas <dominykas.baronas@mif.stud.vu.lt>
2023-05-03 20:13:16 +0300

    Removing react-hooks-global-state and its redundant code parts.

--- frontend-webserver/src/pages/index.js
+++ frontend-webserver/src/pages/index.js
@@ -3,18 +3,11 @@ import Main from './Main'
 const Game = lazy(() => import('./Game'))
 const News = lazy(() => import('./News'))
 const Login = lazy(() => import('./Login'))
-import { createGlobalState } from "react-hooks-global-state"
 
-const { getGlobalState, useGlobalState, setGlobalState } = createGlobalState({
-    connectedAccount: ''
-})
 
 export {
     Main,
     Game,
     News,
     Login,
-    getGlobalState,
-    useGlobalState,
-    setGlobalState
 }
