66ef303ee53cb0b3f0356f3e289a728c3c132bfe
Martynas M <muizysmartynas@gmail.com>
2023-05-15 17:10:52 +0300

    fixing coding mistake

--- backend-webserver/middleware/verifyMessage.js
+++ backend-webserver/middleware/verifyMessage.js
@@ -4,12 +4,13 @@ const { User } = require('../models/index')
 const crypto = require('crypto')
 
 const verifyMessage = async (signature, address) => {
-    let { nonce } = await User.findOne({ where: { wallet_address: address } })
-    var message = `Mint NFT to connected account\n\nto: ${address}\nnonce: ${nonce}`
+    let user = await User.findOne({ where: { wallet_address: address } })
+    var message = `Mint NFT to connected account\n\nto: ${address}\nnonce: ${user.nonce}`
     const recoveredAddress = await web3.eth.accounts.recover(message, signature)
 
     if(recoveredAddress == address) {
-        await User.update({ nonce: crypto.randomBytes(64).toString("base64") })
+        let newNonce = crypto.randomBytes(64).toString("base64")
+        await user.update({ nonce: newNonce })
         return true
     } else {
         return false
