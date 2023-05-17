c033f5bfa4be81c4442f23d445b2b3fb5266e008
Martynas M <muizysmartynas@gmail.com>
2023-05-17 18:58:15 +0300

    testing

--- backend-webserver/app/Database.js
+++ backend-webserver/app/Database.js
@@ -62,6 +62,8 @@ const checkUser = async (req, res) => {
 const getData = async (req, res) => {
     const { address } = req.body
 
+    const { name } = await db.PlayerIsland.findOne( { where: { wallet_address: address }, include: { model: db.Island}})
+    console.log(name)
     const { username, data } = await db.User.findOne({ where: { wallet_address: address } })
 
     res.send({ username: username, data: data })
