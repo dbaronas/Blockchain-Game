cd3898c73eca90a5cb0418d90bad06695c9359f0
Martynas M <muizysmartynas@gmail.com>
2023-05-17 19:50:27 +0300

    testing

--- backend-webserver/models/index.js
+++ backend-webserver/models/index.js
@@ -5,10 +5,10 @@ const Item = require('./item')
 const Inventory = require('./player_inventory')
 const PlayerIsland = require('./player_island')
 
-User.belongsToMany(Item, {through: Inventory, foreignKey: 'wallet_address'})
-User.belongsToMany(Island, {through: PlayerIsland, foreignKey: 'wallet_address'})
-Item.belongsToMany(User, {through: Inventory, foreignKey: 'item_id'})
-Island.belongsToMany(User, {through: PlayerIsland, foreignKey: 'island_id'})
+User.belongsToMany(Item, {through: Inventory, foreignKey: 'wallet_address', as: 'inventory'})
+User.belongsToMany(Island, {through: PlayerIsland, foreignKey: 'wallet_address', as: 'island'})
+Item.belongsToMany(User, {through: Inventory, foreignKey: 'item_id', as: 'items'})
+Island.belongsToMany(User, {through: PlayerIsland, foreignKey: 'island_id', as: 'islands'})
 
 module.exports = {
     User,
