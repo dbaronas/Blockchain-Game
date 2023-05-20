9d88cf612b8cd8667eb9a2f9291618e696c506af
Martynas M <muizysmartynas@gmail.com>
2023-05-20 19:58:52 +0300

    Update user.js

--- backend-webserver/models/user.js
+++ backend-webserver/models/user.js
@@ -12,7 +12,7 @@ const User = sequelize.define('users', {
         allowNull: false,
         unique: true
     },
-    creation_date: {
+    creationAt: {
         type: DataTypes.DATE,
         allowNull: false
     },
