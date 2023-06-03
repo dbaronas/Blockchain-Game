adc2cd10cda05a1330959f82ab11de7774c9facb
Martynas M <muizysmartynas@gmail.com>
2023-06-03 20:37:52 +0300

    login page change

--- frontend-webserver/src/pages/Login.jsx
+++ frontend-webserver/src/pages/Login.jsx
@@ -2,6 +2,7 @@ import { login } from '../assets'
 import Authentication from '../components/Authentication'
 import { useLocation } from 'react-router-dom'
 import { useAccount } from "wagmi"
+import styles from '../style'
 
 const Login = () => {
     const { state } = useLocation();
@@ -12,12 +13,21 @@ const Login = () => {
         <div className=' text-white h-[50vh] flex justify-center items-center'>
             {!isConnected
                 ?
-                <div className=' h-[400px] w-[400px] bg-neutral-700 grid place-items-center'>
-                    <p className='text-white'>
-                        Please login in order to proceed
-                    </p>
-                    <img src={login} alt="login" />
-                    <Authentication />
+                <div className='flex flex-row justify-between items-center p-1'>
+                    <div className='max-w-[600px] max-h-[450px] bg-neutral-700 flex flex-col justify-center items-center xl:px-6 sm:px-16 px-6'>
+                        <p className='text-white p-5'>
+                            Please login in order to proceed
+                        </p>
+                        <h1 className=' text-start text-[40px] text-red-600 font-vt323 font-semibold'>IMPORTANT!</h1>
+                        <ul className='marker:text-white list-disc pb-5'>
+                            <li className={`${styles.paragraph} max-w-[500px] text-white`}>We do not gain access to any of your private data like private keys or passwords.</li>
+                            <li className={`${styles.paragraph} max-w-[500px] text-white`}>We only ask for your public address, account balance and activity.</li>
+                            <li className={`${styles.paragraph} max-w-[500px] text-white`}>Wallet connection is handled by WalletConnect communications protocol.</li> 
+                        </ul>
+                        <div className='pb-10'>
+                            <Authentication />
+                        </div>
+                    </div>
                 </div>
                 : window.location.replace(from.pathname)
             }
