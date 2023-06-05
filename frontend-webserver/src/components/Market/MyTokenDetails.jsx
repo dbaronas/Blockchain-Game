385d6b4590f59ef31e61a27582da87d638b96849
Baronas Dominykas <dominykas.baronas@mif.stud.vu.lt>
2023-06-05 20:35:06 +0300

    Responsive design
    Responsive design for Market components.

--- frontend-webserver/src/components/Market/MyTokenDetails.jsx
+++ frontend-webserver/src/components/Market/MyTokenDetails.jsx
@@ -45,30 +45,30 @@ const MyTokenDetails = () => {
   
 
   return (
-    <div className="text-white flex flex-row p-3 justify-center items-center">
-      <div className="basis-2/6 p-2">
+    <div className="text-white flex flex-row p-3 justify-center items-center max-[700px]:flex-col">
+      <div className="basis-2/6 p-2 max-[700px]:flex max-[700px]:justify-center max-[700px]:items-center">
         <img
           src={image}
           alt={name}
-          className="w-full shadow-yellow-500 border-2 rounded-lg shadow-lg border-yellow-500"
+          className="max-[700px]:w-2/3 lg:w-10/12 w-full shadow-yellow-500 border-2 rounded-lg shadow-lg border-yellow-500"
         />
       </div>
       <div className="flex flex-col gap-4 items-center justify-center basis-3/6 p-3">
-        <h1 className="text-gold2 font-vt323 font-normal text-[1.5rem]">
+        <h1 className="text-gold2 font-vt323 font-normal text-[1.5rem] lg:text-[1.8rem]">
           <i className="fa fa-check-circle text-gold3 p-1 text-[1rem]"></i>MetaOcean
         </h1>
-        <p>Token ID: #{tokenId}</p>
-        <p>
+        <p className="lg:text-[1.2rem]">Token ID: #{tokenId}</p>
+        <p className="lg:text-[1.2rem]">
           Owned by <span className="text-gold3">{walletAddress}</span>
         </p>
-        <div className="flex justify-center flex-col border border-yellow-500 gap-3 rounded-lg shadow-yellow-500 shadow-md">
+        <div className="lg:text-[1.2rem] flex justify-center flex-col border border-yellow-500 gap-3 rounded-lg shadow-yellow-500 shadow-md">
           <p className="text-gold3 border border-yellow-500 p-2">
             <i className="fas fa-align-justify pr-2 pb-2 text-gold3"></i>
             Description
           </p>
           <p className="p-2">{description}</p>
           {fishing_speed && (
-            <p className="pl-2 pb-2">Fishing speed: {fishing_speed}</p>
+            <p className="pl-2 pb-2 lg:text-[1.2rem]">Fishing speed: {fishing_speed}</p>
           )}
         </div>
         {isOwner && (
